0bc2449238a7e0b4c3089da5595824c099dd6ef0
AndriiMikita <103601417+AndriiMikita@users.noreply.github.com>
2023-12-24 19:07:29 +0200

    Sequrity update

--- api/permissions.py
+++ api/permissions.py
@@ -8,3 +8,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 class IsAuthenticatedOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated or request.method in permissions.SAFE_METHODS
+    
+class IsOwnerOrReadOnlyNoAuth(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user or request.method in permissions.SAFE_METHODS
